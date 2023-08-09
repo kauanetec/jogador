54d2c6dbfe6ce8487f4ee37c05b32f13ea42b7cf
kauan <111579430+kauanetec@users.noreply.github.com>
2023-08-09 11:43:07 -0300

    Add files via upload

---
+++ jogador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kauan_e_jose
+{
+    class Jogador : Program
+    {
+        String nome;
+        int idade;
+        int drible;
+        int velocidade;
+        int chute;
+        int forca;
+
+        public void correr()
+        {
+            int aceleracao = forca + velocidade;
+        }
+        
+        public int chutar()
+        {
+            int potenciachute = forca * chute;
+            return potenciachute;
+        }
+
+        public double tocar()
+        {
+
+           double tocar = forca / 3 + chute / 2;
+            return tocar;
+
+        }
+
+        public double interceptar()
+        {
+            double roubar = velocidade * velocidade + forca / 2;
+            return roubar;
+
+        }
+
+        public int driblar()
+        {
+            int driblar = drible + velocidade * velocidade;
+            return driblar;
+
+        }
+
+    }
+}
